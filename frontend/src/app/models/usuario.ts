7b7007e55a8af5632e65ea73a1de0edc3816ec77
denn95 <deniron13@gmail.com>
2021-11-24 11:13:29 -0600

    solucion de agregar usuario

---
+++ frontend/src/app/models/usuario.ts
@@ -0,0 +1,19 @@
+export class Usuario {
+    idUsuario?:number= 0;
+    nombreUsuario:string = '';
+    correoUsuario:string = '';
+    contrasena:string = '';
+    telefonoUsuario:string = '';
+    direccion:string = '';
+    idRol:number = 0;
+
+    constructor() {
+    this.idUsuario=this.idUsuario;
+    this.nombreUsuario=this.nombreUsuario;
+    this.correoUsuario=this.correoUsuario;
+    this.contrasena=this.contrasena;
+    this.telefonoUsuario=this.telefonoUsuario;
+    this.direccion=this.direccion;
+    this.idRol = this.idRol;
+    }
+}
---
+++ frontend/src/app/service/usuario.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import jwt_decode from "jwt-decode";
+import { Usuario } from '../models/usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+  token:string="";
+  user:Usuario = new Usuario();
+  url:string = environment.baseUrl;
+  constructor(private http:HttpClient){}
+
+  login(usuario:any){
+    let params = JSON.stringify(usuario);
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    return this.http.post(this.url+'Usuario/login-usuario', params, {headers: headers});
+  }
+
+  registro(usuario:any){
+    let params = JSON.stringify(usuario);
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    return this.http.post(this.url+'Usuario/agregar-usuario', params, {headers: headers});
+  }
+
+  saveToken(key: string, token: string){
+  
+  try{
+      localStorage.setItem(key, JSON.stringify(token));
+  }catch(error){
+      console.log("saveToken", error);
+  }
+}
+
+getToken(key:string){
+  try{
+      return localStorage.getItem(key);
+  }catch(error){
+    console.log("getToken", error);
+  }
+  return "";
+}
+
+userLogged(){
+  console.log(this.getToken('token'));
+  this.token = this.getToken('token') as string;
+  var decoded = jwt_decode(this.token);
+  console.log(decoded);
+  return decoded;//Está así porque no funciona con notación de punto
+
+}
+
+getRole(){
+  this.user = this.userLogged() as Usuario;
+  console.log(this.user);
+  return this.user.idRol;
+}
+
+userIsLogged(){
+  if(this.getToken('token')){
+        return true;
+  }else{
+     return false;
+  }
+}
+
+logout(){
+  localStorage.removeItem('token');
+}
+
+}
